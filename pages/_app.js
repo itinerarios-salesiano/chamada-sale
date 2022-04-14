65d8e4d8e3a75582826d6971e0f8882f5957ba7d
AlissonSteffens <ali.steffens@gmail.com>
2022-04-13 21:25:31 -0300

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import { CssBaseline, GeistProvider } from '@geist-ui/core'
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <GeistProvider>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </GeistProvider>
+  )
+}
+
+export default MyApp
